fc6d719b35addead0bba85006c1f79bfc26c4931
Anwin Joselyn <anwin.joselyn@clickpost.in>
2022-05-30 17:35:15 +0530

    configs

---
+++ pages/_document.tsx
@@ -0,0 +1,89 @@
+import Document, {
+  DocumentContext,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return initialProps;
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Red+Hat+Mono:ital,wght@0,400;0,600;1,400;1,600&display=swap"
+            rel="stylesheet"
+          />
+          <noscript>
+            <link
+              rel="stylesheet"
+              href="https://fonts.googleapis.com/css2?family=Red+Hat+Mono:ital,wght@0,400;0,600;1,400;1,600&display=swap"
+            />
+          </noscript>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest"></link>
+          <link
+            href="https://fonts.googleapis.com/icon?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone"
+            rel="stylesheet"
+          ></link>
+          {/* <script
+              src="https://www.google.com/recaptcha/api.js"
+              async
+              defer
+            ></script> */}
+          {typeof window !== 'undefined' && (
+            <script>
+              {/* {`  const root: any = window.document.documentElement;
+                    const isDarkSet = localStorage.theme && localStorage.theme === 'dark';
+                    const isDarkPreferred = window.matchMedia(
+                      '(prefers-color-scheme: dark)'
+                    ).matches;
+                    if (isDarkSet && isDarkPreferred) {
+                      root.classList.remove(theme);
+                      root.classList.add(theme);
+                    } else {
+                      root.classList.remove(theme);
+                    }
+              `} */}
+            </script>
+          )}
+        </Head>
+        <body className="overscroll-none">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,76 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
+  variants: {
+    extend: {
+      display: ['group-hover'],
+    },
+  },
+  darkMode: 'class',
   theme: {
-    extend: {},
+    fontFamily: {
+      openSans: ['Red Hat Mono', 'monospace'],
+    },
+    colors: {
+      transparent: 'transparent',
+      blueBlack: '#003342',
+      orange: {
+        bright: '#FF4D00',
+        muted: '#F57636',
+      },
+      red: {
+        primary: '#FF2929',
+        bg: '#FF4D000D',
+      },
+      gray: {
+        lightest: '#E6EDF0',
+        light: '#E8E9EB',
+        dark: '#AEBCC2',
+        darkest: '#4C5760',
+        1: '#E5E5E5',
+        2: '#D2D2D2',
+        3: '#CCCCCC',
+        4: '#99A1A7',
+        5: '#808080',
+        6: '',
+      },
+      blue: {
+        lightest: '#B5DFFF',
+        light: '#4AA7ED',
+        muted: '#139ED1',
+        dark: '#147BC9',
+        darkest: '#0280A6',
+      },
+      background: {
+        dark: '#AECAD4',
+        light: '#F7FDFF',
+        darker: '#2D3039',
+        darkest: '#1E2028',
+      },
+      green: {
+        light: '#BFFFDA',
+        bright: '#15C25E',
+        dark: '#209652',
+      },
+      yellow: '#FFFF0D',
+      black: {
+        pure: '#000000',
+        1: '#252525',
+        light: '#4C5760',
+        2: '#333333',
+      },
+      white: '#FFFFFF',
+    },
+    boxShadow: {
+      header: '0px 2px 3px rgba(0, 0, 0, 0.1)',
+    },
+  },
+  variants: {
+    extend: {
+      opacity: ['disabled'],
+    },
   },
   plugins: [],
 };
