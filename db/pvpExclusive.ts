c52d0c406804437d0f935594ef11b8657cdd0c24
Anwin Joselyn <anwin.joselyn@clickpost.in>
2022-06-01 10:34:16 +0530

    and more db entries

---
+++ db/pvpExclusive.ts
@@ -0,0 +1,18 @@
+export const pvpExclusive = [
+  {
+    id: 619,
+    name: 'Mienfoo',
+  },
+  {
+    id: 620,
+    name: 'Mienshao',
+  },
+  {
+    id: 627,
+    name: 'Rufflet',
+  },
+  {
+    id: 628,
+    name: 'Braviary',
+  },
+];
