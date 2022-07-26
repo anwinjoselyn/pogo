a4df8e6a14b93411d8a07bd91fbdf996b50268c0
Anwin Joselyn <anwin.joselyn@clickpost.in>
2022-07-26 16:14:37 +0530

    meaningless commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ const Home: NextPage = () => {
   return (
     <main className="p-8">
       <h1 className="text-center">Welcome to The PogoStop - A fan app for private Pokemon Go info</h1>
-      <div className="flex gap-2 p-4 justify-center items-center my-8">
+      <div className="flex gap-2 p-4 justify-center items-center mt-8">
         {mainCards.map((card: any) => (
           <Card key={card.title} {...card}>
             <div className="text-center py-6">{card.description}</div>
