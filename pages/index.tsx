aeeb407977bdbd63daac7516f32cfd2deb338d8e
Anwin Joselyn <anwin.joselyn@clickpost.in>
2022-07-26 15:44:21 +0530

    index page welcome message

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,12 +6,15 @@ import { mainCards } from '../constants/defaultValues';
 
 const Home: NextPage = () => {
   return (
-    <main className="flex gap-2 p-4 justify-center items-center">
-      {mainCards.map((card: any) => (
-        <Card key={card.title} {...card}>
-          <div className="text-center py-6">{card.description}</div>
-        </Card>
-      ))}
+    <main className="p-8">
+      <h1 className="text-center">Welcome to The PogoStop - A fan app for private Pokemon Go info</h1>
+      <div className="flex gap-2 p-4 justify-center items-center mt-8">
+        {mainCards.map((card: any) => (
+          <Card key={card.title} {...card}>
+            <div className="text-center py-6">{card.description}</div>
+          </Card>
+        ))}
+      </div>
     </main>
   );
 };
